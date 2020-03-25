064a70783b3d630ce8d2deaa26863cd2081273e5
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2020-03-25 21:23:42 +0900

    :gem: bundle gem active_record_scope_analyzer

---
+++ lib/active_record_scope_analyzer.rb
@@ -0,0 +1,6 @@
+require "active_record_scope_analyzer/version"
+
+module ActiveRecordScopeAnalyzer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/active_record_scope_analyzer/version.rb
@@ -0,0 +1,3 @@
+module ActiveRecordScopeAnalyzer
+  VERSION = "0.1.0"
+end
